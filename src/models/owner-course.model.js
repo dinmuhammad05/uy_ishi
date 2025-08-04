978d2d741f82945d351b7d27633a11811c2e27de
Dinmuhammad Qosimov <qosimovmirjalol829@gmail.com>
2025-08-04 10:59:47 +0500

    .

--- src/models/owner-course.model.js
+++ src/models/owner-course.model.js
@@ -5,7 +5,7 @@ const OwnerCourseSchema = new Schema(
         email: { type: String, required: true, unique: true },
         username: { type: String, required: true, unique: true },
         fullName: { type: String, required: true },
-        isActive: { type: Boolean, default: false },
+        isActive: { type: Boolean, default: true },
         hashedPassword: { type: String, required: true },
         wallet: { type: Number, default:0 },
         experience: { type: String },
